ad5f568b3413239782e78d415e842263e803a7bb
Kishan A <164038197+scriptWithKishan@users.noreply.github.com>
2024-07-21 17:08:32 +0530

    Delete HealthCare-master directory

--- HealthCare-master/medical/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import CustomUser, Patient, Doctor, Prescription, Report
-
-# Register your models here.
-admin.site.register(CustomUser)
-admin.site.register(Patient)
-admin.site.register(Doctor)
-admin.site.register(Prescription)
-admin.site.register(Report)
